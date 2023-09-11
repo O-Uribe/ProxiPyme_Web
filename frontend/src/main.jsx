ceacf60ed9db9c09be3ab169842c711c1aae5258
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-11 00:34:47 -0300

    "Detalles"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <h1>Bienvenido a Proxi Pyme</h1>
   </React.StrictMode>,
 )
 
