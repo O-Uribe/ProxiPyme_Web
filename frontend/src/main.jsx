cd2146b0378de2901c7424bd2be536166c36e6cc
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-10 22:55:45 -0300

    "Probando"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+<index className="html"></index>
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <h1>Pagina De Inicio</h1>
