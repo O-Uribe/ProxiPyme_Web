89ebe758053f2f9c26dade4488d75868ed8f0549
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-11 00:12:08 -0300

    "Probando"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import 'bulma/css/bulma.min.css';
 
 <index className="html"></index>
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1>Pagina De Inicio</h1>
-  </React.StrictMode>,
-)
+
+
+
