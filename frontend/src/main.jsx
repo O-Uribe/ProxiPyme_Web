02b30188062f094f7029432b9c205f08b2d03f42
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-11 00:29:33 -0300

    "Modificacion"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bulma/css/bulma.min.css';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
