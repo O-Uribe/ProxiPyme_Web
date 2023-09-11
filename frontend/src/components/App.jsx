ead8fc57635506799879b289356b2aab78c0c466
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-11 00:46:40 -0300

    "Arreglo"

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export const App = () => {
+   return (
+       <div>
+        <h1>Bienvenidos a Proxi Pyme</h1>
+        <p>Formulario De Registro</p>
+
+       </div>
+          
+    )
+
+
+
+
+}
