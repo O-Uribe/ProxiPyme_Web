e7fea3abb3ce4e69674890e2daa802f4da0f7cdd
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-11 00:48:41 -0300

    "Arreglo"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import 'bulma/css/bulma.min.css';
 import ReactDOM from 'react-dom/client'
+import { App } from './components/App';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Bienvenido a Proxi Pyme</h1>
+    <App/>
   </React.StrictMode>,
 )
 
