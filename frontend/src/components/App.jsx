6e10a0ee5414e3df12078a716b0a90ba02e60473
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-14 23:41:54 -0300

    "Formulario En Prueba"

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import 'bulma/css/bulma.min.css';
+import { Formulario } from "./Formulario";
 import './App.css';
 export const App = () => {
    return (
        <div>
         <h1 class="title">Bienvenidos a Proxi Pyme</h1>
-        <h2 class="subtitle">Formulario De Registro</h2>
+        <h2 class="title">Formulario De Registro</h2>
         
-    
-
+        
+         <Formulario/>
 
        </div>
           
---
+++ frontend/src/components/Formulario.jsx
@@ -0,0 +1,61 @@
+import "./Formulario.css"
+import { useState } from "react"
+
+
+export function Formulario(){
+
+    const [nombre, setNombre] = useState("")
+    const [contraseña, setContraseña] = useState("")
+    const [error, setError] = useState(false)
+
+    
+    const handleSubmit = () =>{
+
+    e.preventDefault()
+
+     if(nombre == "" || contraseña == ""){
+
+        setError(true)
+        return
+     } 
+
+    } 
+    
+    return(
+
+<section>
+    <h1 class="title">Login</h1>
+
+    <form 
+    className="formulario"
+    onSubmit={handleSubmit}
+    >
+
+         
+         <input 
+         type="text" 
+         value={nombre}
+         onChange={e => setNombre(e.target.value)}
+         />
+
+
+         <input 
+         type="password" 
+         value={contraseña}
+         onChange={e => setContraseña(e.target.value)}
+         />
+
+
+
+         <button>Iniciar Sesion</button>
+
+    </form>
+
+    {error && <p>Todos los campos son obligatorios</p> }
+
+</section>
+
+)
+
+
+}
