33d7764d7f29c4b32b9c0f57cad57c7b534e8a78
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-22 22:02:07 -0300

    "Arreglo"

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,9 +10,7 @@ export function Home({user, setUser}) {
        return(
 
        <div>
-           <h1 class="title">Bienvenido <span class="icon">
-           <i class="fas fa-home"></i>
-           </span></h1>
+           <h1 class="title">Bienvenido</h1>
            <h2 class="title">{user}</h2>
            <button class="button is-link is-rounded" onClick={handLeLogout}>Cerrar Sesion</button>
 
