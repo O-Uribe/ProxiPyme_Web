7aedb7aafccba8e5a4f1a3b0a4fb891bc9b4c6b8
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-22 21:53:23 -0300

    "Icono"

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,6 +11,12 @@ export function Home({user, setUser}) {
 
        <div>
            <h1 class="title">Bienvenido</h1>
+           <span class="icon-text">
+  <span class="icon">
+    <i class="fas fa-home"></i>
+  </span>
+  <span>Home</span>
+</span>
            <h2 class="title">{user}</h2>
            <button class="button is-link is-rounded" onClick={handLeLogout}>Cerrar Sesion</button>
 
