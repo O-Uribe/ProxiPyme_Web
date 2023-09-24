f192f4e937b3509d41513da64e7a008fdffa64d8
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-24 04:06:31 -0300

    "Edicion"

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -14,13 +14,14 @@ export function Home({user, setUser}) {
           <div class="container">
           <div class="notification is-primary">
           <strong>Bienvenido</strong>
-          </div>
-          </div>
+          
 
-           <h1 class="title">Bienvenido</h1>
+          
            <h2 class="title">{user}</h2>
            <button class="button is-link is-rounded" onClick={handLeLogout}>Cerrar Sesion</button>
-
+        
+           </div>
+          </div>
 
        </div>
 
