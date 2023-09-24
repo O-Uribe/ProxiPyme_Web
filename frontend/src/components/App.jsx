77bac867f4de98cf31f0fe7a8153802945e2e3b3
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-24 03:37:44 -0300

    "Edicion"

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -12,6 +12,14 @@ export const App = () => {
   return (
        <div>
         <h1 class="title">Bienvenidos a Proxi Pyme</h1>
+        <nav class="breadcrumb is-centered" aria-label="breadcrumbs">
+  <ul>
+    <li><a href="#">Bulma</a></li>
+    <li><a href="#">Documentation</a></li>
+    <li><a href="#">Components</a></li>
+    <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
+  </ul>
+</nav>
         <h2 class="title">Formulario De Registro</h2>
         
         {
