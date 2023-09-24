147d6acd3a8a3c5cea2a5dfebd499d2d6c66dd47
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-09-24 04:04:15 -0300

    "Contenedor"

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,6 +10,13 @@ export function Home({user, setUser}) {
        return(
 
        <div>
+           
+          <div class="container">
+          <div class="notification is-primary">
+          <strong>Bienvenido</strong>
+          </div>
+          </div>
+
            <h1 class="title">Bienvenido</h1>
            <h2 class="title">{user}</h2>
            <button class="button is-link is-rounded" onClick={handLeLogout}>Cerrar Sesion</button>
