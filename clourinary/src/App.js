bc44de0bf907d98620e8e8b9cb68ef1c9dc14f73
Xechors <111018707+JavierMonsalvez@users.noreply.github.com>
2023-09-25 20:33:56 -0300

    Intento de Subida de imagenes
    Se intenta usar cloudrinary para la subida de las imagenes

---
+++ clourinary/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TestSubidas from './Components/TestSubidas';
+
+function App() {
+  return (
+    <div>
+      <TestSubidas/>
+    </div>
+  );
+}
+
+export default App;
---
+++ clourinary/src/Components/TestSubidas.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TestSubidas = (props) => {
+    return ( <div>
+        holaa
+    </div> );
+}
+ 
+export default TestSubidas;
