b3a47814f6e29be110b1c57857d2d693a183c419
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-10-14 22:38:33 -0300

    "Estilo"

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bulma/css/bulma.min.css';
-import "./Perfil.css";
+import "./Dashboard.css";
 import { useState } from "react";
 import { FcShop } from "react-icons/fc";
 import { createChart } from 'lightweight-charts';
