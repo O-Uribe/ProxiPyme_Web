3a9a53a35d5b86d9fc66d807fe37c0f2d6613e4d
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-10-16 22:43:24 -0300

    "Listo"

--- frontend/src/components/Mapa.jsx
+++ frontend/src/components/Mapa.jsx
@@ -15,24 +15,18 @@ export const Mapa = () => {
 return (
 
 
-
-   <Map center={[52.6376, -1.135171]} zoom={12}>
-       
-       <TileLayer
-       
-       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-       
-       
-       
-       
-       
-       
-       
-       
-       />
-
-    </Map>
+<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+   
 
 
 
