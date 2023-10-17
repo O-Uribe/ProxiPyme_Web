74cad59ddc6d28eb32dc1e7c0fc06de2f569cd8b
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-10-16 22:14:30 -0300

    "Listo"

--- frontend/src/components/Mapa.jsx
+++ frontend/src/components/Mapa.jsx
@@ -19,7 +19,8 @@ return (
        <TileLayer
        
        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> 
+       contributors'
        
        
        
