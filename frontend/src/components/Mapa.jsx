b6bc1ffaa6a162c4e0f3162e4d211f0408c15a83
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-10-16 22:36:58 -0300

    "Listo"

--- frontend/src/components/Mapa.jsx
+++ frontend/src/components/Mapa.jsx
@@ -6,7 +6,7 @@ import { FcShop } from "react-icons/fc";
 import { createChart } from 'lightweight-charts';
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
 import {Icon} from "leaflet";
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+
 
 
 export const Mapa = () => {
