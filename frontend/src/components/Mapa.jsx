9ddace32759d8e845dff3b8258a946feb70a4202
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-10-16 21:34:32 -0300

    "Creando mapa"

---
+++ frontend/src/components/Mapa.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import 'bulma/css/bulma.min.css';
+import "./Mapa.css";
+import { useState } from "react";
+import { FcShop } from "react-icons/fc";
+import { createChart } from 'lightweight-charts';
+
+
+export const Mapa = () => {
+
+
+return (
+
+<h1>Aqui va el mapa</h1>
+
+
+
+
+)
+
+
+
+}
