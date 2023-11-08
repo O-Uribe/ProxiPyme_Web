a9b5bebfe40020476bd4911653c537575e82b026
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-11-08 16:16:14 -0300

    "Nuevo Mapa"

--- frontend/src/component/Map.jsx
+++ frontend/src/component/Map.jsx
@@ -1,37 +1,61 @@
-// useRef se usa para acceder a los elementos del DOM directamente en React.
-import React, { useRef } from "react";
-// Componentes utilizados para un mapa y una capa de mosaicos
-import { MapContainer, TileLayer, Marker} from "react-leaflet";
-// Estilos de la libreria leaflet
+import React, { useState, useEffect } from "react";
+import { Map, TileLayer } from "react-leaflet";
+import data from "../assets/data.json";
+import Markers from "./VenueMarkers";
+
+import { useLocation, useHistory } from "react-router-dom";
+
 import "leaflet/dist/leaflet.css";
-import useSwr from "swr";
+
+const MapView = (props) => {
+  const [state, setState] = useState({
+    currentLocation: { lat: 52.52437, lng: 13.41053 },
+    zoom: 13,
+    data,
+  });
+
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (location.state.latitude && location.state.longitude) {
+      const currentLocation = {
+        lat: location.state.latitude,
+        lng: location.state.longitude,
+      };
+      console.log(state);
+      setState({
+        ...state,
+        data: {
+          venues: state.data.venues.concat({
+            name: "new",
+            geometry: [currentLocation.lat, currentLocation.lng],
+          }),
+        },
+        currentLocation,
+      });
+      history.replace({
+        pathname: "/map",
+        state: {},
+      });
+    }
+  }, [location]);
 
 
-const fetcher = (...args) => fetch(...args).then(response => response.json());
+return (
+        
+
+<Map center={state.currentLocation} zoom={state.zoom}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Markers venues={state.data.venues} />
+    </Map>
 
 
-
-function Map() {
-   
-    const url = "https://data.police.uk/api/crimes-street/all-crime? latitud =52.629729& longitud =-1.131592& fecha =2017-01";
-    const {data, error} = useSwr(url, fetcher);
-    const crimes = data && !error ? data.slice(0,100) :  [];
-
-
-
-    return (
-        // Contenedor de mapa con el centro de coordenadas, un zoom y un tamaño
-        <MapContainer center={[52.629729, -1.131592]} zoom={10}  style={{ height: "450px", width: "100%" }}>
-            {/* Capa de mosaicos */}
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-
-
-            {crimes.map(crime => <Marker key={crime.id} position={[crime.location.Latitud, crime.location.Longitud]} />)}
-        </MapContainer>
+      
     );
 };
 
-export default Map
+export default MapView;
