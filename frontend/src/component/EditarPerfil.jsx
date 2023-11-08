e4e37cd995f8fab3172f23ea46b3ff68bb8ede07
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-11-08 20:20:32 -0300

    "Listo"

--- frontend/src/component/EditarPerfil.jsx
+++ frontend/src/component/EditarPerfil.jsx
@@ -49,7 +49,13 @@ export function EditarPerfil({ setUser }){
                onChange={e => setContraseña(e.target.value)}
                />
 
-
+          <div class="field">
+          <label class="label">Label</label>
+          <div class="control">
+          <input class="input" type="text" placeholder="Text input"/>
+          </div>
+          <p class="help">This is a help text</p>
+          </div>
       
       
       
