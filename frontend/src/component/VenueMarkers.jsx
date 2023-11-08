16304841bfc551afd8ad025bf7b775aa430abc1f
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-11-08 17:01:06 -0300

    "Listo"

--- frontend/src/component/VenueMarkers.jsx
+++ frontend/src/component/VenueMarkers.jsx
@@ -5,7 +5,7 @@ import MarkerPopup from "./MarkerPopup";
 
 const VenueMarkers = (props) => {
   const { venues } = props;
-  const markers = venues.map((venue, i) => (
+  const markers = venues.mapa((venue, i) => (
     <Marker key={i} position={venue.geometry} icon={VenueLocationIcon}>
       <MarkerPopup data={venue} />
     </Marker>
