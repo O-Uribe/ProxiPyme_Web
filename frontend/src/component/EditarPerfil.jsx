66fd5aee1d471c7e5a6803890530140faff267f9
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-11-08 19:49:23 -0300

    "Editar"

--- frontend/src/component/EditarPerfil.jsx
+++ frontend/src/component/EditarPerfil.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function EditarPerfil () {
     return (
 
-    <h1>Editar</h1>
+        <div class="field">
+        <label class="label">Label</label>
+        <div class="control">
+          <input class="input" type="text" placeholder="Text input">
+        </div>
+        <p class="help">This is a help text</p>
+      </div>
 
       
     )
