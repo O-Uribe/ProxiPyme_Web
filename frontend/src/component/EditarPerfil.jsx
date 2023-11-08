06dd236ac07254c68ebb8eed264cb272cc31aeb3
JJ3972433 <74887887+JJ3972433@users.noreply.github.com>
2023-11-08 19:55:27 -0300

    "Listo"

--- frontend/src/component/EditarPerfil.jsx
+++ frontend/src/component/EditarPerfil.jsx
@@ -1,16 +1,62 @@
 import React from 'react';
+import { useState } from "react"
+import { FcShop } from "react-icons/fc";
 
-function EditarPerfil () {
-    return (
 
-        <div class="field">
-        <label class="label">Label</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Text input">
-        </div>
-        <p class="help">This is a help text</p>
-      </div>
+export function EditarPerfil({ setUser }){
 
+
+          const [nombre, setNombre] = useState("")
+          const [contraseña, setContraseña] = useState("")
+          const [error, setError] = useState(false)
+      
+          
+          const handleSubmit = (e) =>{
+      
+          e.preventDefault()
+      
+           if(nombre == "" || contraseña == ""){
+      
+              setError(true)
+              return
+           } 
+           setError(false)
+      
+           setUser([nombre])
+          } 
+          
+          return(
+      
+      <section>
+          <h1 class="title">Login</h1>
+      
+          <form 
+          className="formulario"
+          onSubmit={handleSubmit}
+          >
+      
+               
+               <input 
+               type="text" 
+               value={nombre}
+               onChange={e => setNombre(e.target.value)}
+               />
+      
+      
+               <input 
+               type="password" 
+               value={contraseña}
+               onChange={e => setContraseña(e.target.value)}
+               />
+      
+      
+      
+               <button class="button is-link is-rounded">Iniciar Sesion</button>
+      
+          </form>
+      
+          {error && <p>Todos los campos son obligatorios.</p>}
+      </section>
       
     )
 }
