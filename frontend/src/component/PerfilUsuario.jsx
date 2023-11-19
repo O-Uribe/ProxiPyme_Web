3c96d961ea37fe2a75277309aa839e491a2a107b
Xechors <javierotobkn@gmail.com>
2023-11-19 02:29:32 -0300

    Cloudinary
    Vista de imagen del cloudinary

--- frontend/src/component/PerfilUsuario.jsx
+++ frontend/src/component/PerfilUsuario.jsx
@@ -1,6 +1,6 @@
 import React from 'react' ; 
 import Navbar from './NavbarPrincipal';
-import Footer from './FooterPrincipal';
+import Footer from "./FooterPrincipal";
 
 function PerfilUsuario() {
   return (
@@ -14,17 +14,27 @@ function PerfilUsuario() {
                 </div>
                 <div className="column is-2">
                     <figure className="image is-128x128">
-                        <img src="https://www.grafitos.com.co/wp-content/uploads/2021/07/brillantina-celetes-tornasol-900x900.jpg" alt="imagen" className="is-rounded" />
+                        <img src="https://res.cloudinary.com/dsspspjhj/image/upload/v1700371252/venom_3840x2160_xtrafondos.com_dce1z2.jpg" alt="imagen" className="is-rounded" />
                     </figure>
                 </div>
                 <div className="column is-5">
                     <h1 className="title has-text-black">Datos</h1>
                     <aside className="is-medium menu">
                         <p className="menu-label has-text-black">Datos</p>
+                        <p className="menu-label has-text-black">
+                            <i className="fa fa-location dot"></i>
+                        </p>
                     </aside>
                 </div>
             </div>
+
+            <hr className="has-background-danger ml-6 mr-6" />
+
+            <h1 className="title has-text-black">Cositas</h1>
         </section>
+
+        <Footer></Footer>
+
     </div>
   )
 }
